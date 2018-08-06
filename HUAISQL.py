935acb73ed0ab23a92a3bcdcc0f9d4832ee2bfc6
1019157263 <1019157263@qq.com>
2018-08-06 10:34:39 +0800

    做完安装程序

--- HUAISQL.py
+++ HUAISQL.py
@@ -1,5 +1,9 @@
 import ast
 import time
+from pylsy import pylsytable
+import random
+
+
 #su=[ast.literal_eval(i) for i in open('su.txt','r')]#解析数据
 su=[eval(i) for i in open('su.txt','r')]#解析数据
 
@@ -13,6 +17,18 @@ def xian():
        n+=1
     #print('\n'.join([str(o) for o in su]))#显示全部
     print('*'*50)
+    
+def xian_n():
+   attributes=["标题","内容","时间"]
+   table=pylsytable(attributes)
+   table.add_data("标题",[i['name'][0:5] for i in su])
+   table.add_data("内容",[i['pwd'][0:8] for i in su])
+   table.add_data("时间",[i['nid'][len(i['nid'])-8:len(i['nid'])] for i in su])
+   print(table)
+
+
+
+
     
 def xie(name,pwd):
 #增加账号
--- githubbool.py
+++ githubbool.py
@@ -1,14 +1,17 @@
 from jinja2 import Template
 import os
 from HUAISQL import *
-f = open('/sdcard/qpython/index.html', 'r')
+f = open('index.html', 'r')
 f=f.read()
 #print(f)
 template = Template(f)
-k=template.render(li=su)
-l=open('/sdcard/qd/1019157263.github.io/index.html','w').write(k)
-os.system('cd /sdcard/qd/1019157263.github.io')
+k=template.render(li=su[::-1])
+
+dict=eval(open('index.config','r').read())
+
+l=open(dict['dz']+'index.html','w').write(k)
+os.chdir(dict['dz'])
 os.system('pwd')
 os.system('git add .')
-os.system('git commit -m '+input('请输入版本号'))
+os.system('git commit -m 自动提交')
 os.system('git push -f origin master')
